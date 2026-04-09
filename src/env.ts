c3368378e772a8242651c302b28a1ec7f193c591
kay30kim <kay30kim@gmail.com>
2026-04-08 22:55:32 -0700

    Add Kakao SDK

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const NAVER_MAP_CLIENT_ID = import.meta.env.VITE_NAVER_MAP_CLIENT_ID
-export const KAKAO_SDK_JS_KEY = import.meta.env.VITE_KAKAO_SDK_JS_KEY
+export const KAKAO_SDK_JS_KEY = "8e9cfbee56e369f52ea9e11336d9e720"//import.meta.env.VITE_KAKAO_SDK_JS_KEY
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export const STATIC_ONLY = import.meta.env.VITE_STATIC_ONLY === "true"
