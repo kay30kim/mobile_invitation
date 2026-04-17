ea2bdb769663594cb33da9ac6f10b5b1406dc260
kay30kim <kay30kim@gmail.com>
2026-04-16 23:49:25 -0700

    Error fix in index.ts in pic

--- src/images/index.ts
+++ src/images/index.ts
@@ -12,6 +12,10 @@ import image9 from "./image9.jpg"
 import image10 from "./image10.jpg"
 import image11 from "./image11.jpg"
 import image12 from "./image12.jpg"
+import image13 from "./image13.jpg"
+import image14 from "./image14.jpg"
+import image15 from "./image15.jpg"
+import image16 from "./image16.jpg"
 
 export const COVER_IMAGE = coverImage
 export const MAIN_COVER_IMAGE20 = maincoverImage20
@@ -28,5 +32,9 @@ export const GALLERY_IMAGES = [
   image9,
   image10,
   image11,
-  image12
+  image12,
+  image13,
+  image14,
+  image15,
+  image16
 ]
